81f73f3f9d3de950b2b066dd97bd77eaed05e2d7
Richard Neumann <r.neumann@homeinfo.de>
2022-03-30 13:49:15 +0200

    Add basic script

---
+++ js/index.mjs
@@ -0,0 +1,60 @@
+/*
+  index.mjs - ComCat password reset JavaScript functions.
+
+  (C) 2022 HOMEINFO - Digitale Informationssysteme GmbH
+
+  This library is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+  Maintainer: Richard Neumann <r dot neumann at homeinfo period de>
+*/
+'use strict';
+
+import { requests } from 'https://javascript.homeinfo.de/requests.mjs';
+
+
+const URL = 'https://comcat.homeinfo.de/pwreset/confirm';
+
+
+function getURLParams () {
+    return new URLSearchParams(window.location.search);
+}
+
+
+function getNonce () {
+    return getURLParams().get('nonce');
+}
+
+
+function resetPassword () {
+    return requests.post(URL, {'nonce': getNonce()});
+}
+
+
+function getMessage (response) {
+    if response.status != 200:
+        return 'Ihr Passwort konnte leider nicht zurückgesetzt werden.';
+
+    return 'Wir haben Ihnen Ihr neues Passwort per E-Mail zugesendet';
+}
+
+
+function showMessage (response) {
+    const messageContainer = document.getElementById('message');
+    messageContainer.textContent = getMessage(response);
+}
+
+
+export function init () {
+    resetPassword().then(showMessage);
+}
